55e0dc3f37d124d447a7a6db41246b4eabd5b7fb
Burak Olgun <burakolgun91@gmail.com>
2017-08-02 17:27:12 +0300

    Controller updated.

--- app/Model/Department.php
+++ app/Model/Department.php
@@ -8,4 +8,32 @@ class Department extends Model
 {
     protected $table = 'department';
     public $timestamps = false;
+
+    public static function getAllDepartmentsAction()
+    {
+        $departments = Department::all();
+        return $departments;
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Database\Eloquent\Collection|Model|null|static|static[]
+     */
+    public static function findDepartmentByIdAction($id)
+    {
+        $department = Department::find($id);
+        return $department;
+    }
+
+    /**
+     * @param $departmentId
+     * @return static
+     */
+    public static function getThisDepartmentsUsersAction($departmentId)
+    {
+        $totalUsers = User::all()->where('department_id' , '=' , $departmentId);
+        return $totalUsers;
+    }
 }
+
+
