a2c1622577384231dd955e9b1295d2b5eb6595b5
BusraG <busragumusel@gmail.com>
2017-08-02 23:12:58 +0300

    Added Mail Controller.

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MailController extends Controller
+{
+    //
+}
