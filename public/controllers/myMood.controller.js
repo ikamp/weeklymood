140538900d04177494d48c697f2dcae0cedb16e6
Burak Olgun <burakolgun91@gmail.com>
2017-08-08 19:56:05 +0300

    Stacked chart added for mymood page.

--- public/controllers/myMood.controller.js
+++ public/controllers/myMood.controller.js
@@ -2,5 +2,22 @@ angular.module('weeklyMood')
     .controller('MyMoodController', myMoodController);
 
 function myMoodController($scope, DataService) {
-    $scope.name = 'Yalcin T';
-}
+     $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    $scope.type = 'StackedBar';
+    $scope.series = ['2015', '2016'];
+    $scope.options = {
+      scales: {
+        xAxes: [{
+          stacked: true,
+        }],
+        yAxes: [{
+          stacked: true
+        }]
+      }
+    };
+
+    $scope.data = [
+      [65, 59, 90, 81, 56, 55, 40],
+      [28, 48, 40, 19, 96, 27, 100]
+    ];
+  };
